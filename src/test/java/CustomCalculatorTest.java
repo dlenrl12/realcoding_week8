557d539c63d925f0a636df020793601d83240072
DESKTOP-E5Q7HVL\dlenr <dlenrl67@gmail.com>
2021-04-22 22:49:15 +0900

    realcoding

---
+++ src/test/java/CustomCalculatorTest.java
@@ -0,0 +1,54 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CustomCalculatorTest {
+
+    private CustomCalculator customCalculator;
+
+    //더하기 테스트 작성
+    @Test
+    public void addTest() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.add(10,10);
+        assertTrue(result == 20);
+        assertFalse(result == 19);
+        System.out.println("result :: " + result);
+    }
+
+    //빼기 테스트 작성
+    @Test
+    public void testsubtract() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.subtract(23,10);
+        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 13);
+        assertFalse(result == 12);
+        System.out.println("result :: " + result);
+    }
+
+    //곱하기 테스트 작성
+    @Test
+    public void testmultiply() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.multiply(5,9);
+        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 45);
+        assertFalse(result == 44);
+        System.out.println("result :: " + result);
+    }
+
+    //나누기 테스트 작성
+    @Test
+    public void divide() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.divide(25,5);
+        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 5);
+        assertFalse(result == 6);
+        System.out.println("result :: " + result);
+    }
+}
